38fe111733b946f52c67051940b8f01232af2748
khoaphamm <116829205+khoaphamm@users.noreply.github.com>
2024-03-09 17:40:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -10,6 +10,12 @@ var server = http.createServer(function (req, res) {
   res.end('Hello World\n');
 });
 
-server.listen(8080);
+
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
+
+
+
 
 
