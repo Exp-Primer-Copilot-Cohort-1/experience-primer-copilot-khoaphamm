773fd0a47add02c8f84ce328ceee40a3767f3727
khoaphamm <116829205+khoaphamm@users.noreply.github.com>
2024-03-09 17:33:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create a web server 
+// Create a web server that's going to send a response of "Hello World" for every request it receives
+// The server should listen on port 8080
+// Use the createServer method from the http module
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+
+server.listen(8080, '');
+
